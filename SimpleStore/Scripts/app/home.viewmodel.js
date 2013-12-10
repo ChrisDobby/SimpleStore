3973ecc8c471445fa354d2b90edd0b1c10efe4ab
Chris Dobson <dobby@chrisjdobson.co.uk>
2013-12-10 19:35:58 +0000

    Can now enter quantity to add to basket

--- SimpleStore/Scripts/app/home.viewmodel.js
+++ SimpleStore/Scripts/app/home.viewmodel.js
@@ -1,7 +1,18 @@
-﻿function HomeViewModel(app, dataModel) {
+﻿function OrderProduct(product, q) {
+    var prod = this;
+
+    prod.product = product;
+    prod.quantity = ko.observable(q);
+}
+
+function HomeViewModel(app, dataModel) {
     var self = this;
 
-    self.products = app.products;
+    self.products = ko.observableArray([]);
+    for (var p = 0; p < app.products().length;p++) {
+        self.products.push(new OrderProduct(app.products()[p], 1));
+    }
+
     self.basket = dataModel.basket;
 
     self.productTotal = ko.computed(function () {
@@ -17,20 +28,23 @@
         return self.basket().length == 0;
     });
 
-    self.addToBasket = function (product) {
+    self.addToBasket = function (orderProduct) {
+        if (orderProduct.quantity() <= 0) {
+            return;
+        }
         var item;
         for (var i = 0; i < self.basket().length; i++) {
-            if (self.basket()[i].product.Id == product.Id) {
+            if (self.basket()[i].product.Id == orderProduct.product.Id) {
                 item = self.basket()[i];
                 break;
             }
         }
 
         if (item == null) {
-            self.basket.push(new BasketItem(1, product));
+            self.basket.push(new BasketItem(orderProduct.quantity(), orderProduct.product));
         } else {
             var q = item.quantity();
-            q++;
+            q += parseInt(orderProduct.quantity());
             item.quantity(q);
         }
     };
